30eb63cff79d9912fd003ef9916496baeac05154
gamallo <pablo.gamallo@gmail.com>
2025-03-15 22:52:27 +0100

    new run

--- mean/run_mean.sh
+++ mean/run_mean.sh
@@ -1,9 +1,9 @@
 #cat ../kullbackleibler/yy |./mean.perl ../perplexity/xx |./mean.perl ../ranking/zz |./mean.perl ../matrix/ww |sort -g > mean.txt
 
-cat ../perplexity/xx |gawk '{count+=1}{print count, $0}' > results/xx
-cat ../kullbackleibler/yy |gawk '{count+=1}{print count, $0}' > results/yy
-cat ../ranking/zz |gawk '{count+=1}{print count, $0}' > results/zz
-cat ../matrix/ww |gawk '{count+=1}{print count, $0}' > results/ww
+cat ../perplexity/xx |awk '{count+=1}{print count, $0}' > results/xx
+cat ../kullbackleibler/yy |awk '{count+=1}{print count, $0}' > results/yy
+cat ../ranking/zz |awk '{count+=1}{print count, $0}' > results/zz
+cat ../matrix/ww |awk '{count+=1}{print count, $0}' > results/ww
 
 cat results/* |./mean_rank.perl  |sort -g > mean.txt
 
