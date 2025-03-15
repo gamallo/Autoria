b4d6bef957fd43fcf6193ff16352b6140645d288
gamallo <pablo.gamallo@gmail.com>
2025-03-15 21:01:25 +0100

    new run

---
+++ remove.sh
@@ -0,0 +1,8 @@
+# Remove files from directories
+rm -f corpus/train/*
+rm -f corpus/test/*
+rm -f perplexity/models/*
+rm -f perplexity/corpora/test/*
+rm -f perplexity/corpora/train/*
+rm -f kullbackleibler/test/*
+rm -f kullbackleibler/train/*
